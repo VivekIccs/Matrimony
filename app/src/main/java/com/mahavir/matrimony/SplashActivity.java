417466c47525e0590d20066c28fedffd0a723081
viyadav <vivekkumar.yadav@iccs.ac.in>
2017-06-13 17:25:41 +0530

    new things added to login activity

--- app/src/main/java/com/mahavir/matrimony/SplashActivity.java
+++ app/src/main/java/com/mahavir/matrimony/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.mahavir.matrimony;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        } , 2000);
     }
 }
